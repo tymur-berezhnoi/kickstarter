9f8ca2b9e9500593c455988530491c16ac2a3270
TimurJD <tima.peaceworld@gmail.com>
2015-06-26 12:53:39 +0300

    Spring Security

--- src/main/java/com/gojava2/kickstarter/service/UserService.java
+++ src/main/java/com/gojava2/kickstarter/service/UserService.java
@@ -1,11 +1,14 @@
 package com.gojava2.kickstarter.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gojava2.kickstarter.entity.User;
+import com.gojava2.kickstarter.repository.ProjectRepository;
 import com.gojava2.kickstarter.repository.UserRepository;
 
 @Service
@@ -15,8 +18,18 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private ProjectRepository projectRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+	@Transactional
 	public User getUser(String name) {
-		return userRepository.findByName(name);
+		User user = userRepository.findByName(name);
+		user.setProjects(projectRepository.findByUser(user));
+		return user;
 	}
 
 	public void save(User user) {
