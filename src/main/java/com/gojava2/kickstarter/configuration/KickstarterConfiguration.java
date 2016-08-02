17c31dc93ac61e8ffb71026d3a755afb7e5730ab
TimurJD <tima.peaceworld@gmail.com>
2016-08-02 19:50:42 +0300

    removed unused imports

--- src/main/java/com/gojava2/kickstarter/configuration/KickstarterConfiguration.java
+++ src/main/java/com/gojava2/kickstarter/configuration/KickstarterConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
--- src/main/java/com/gojava2/kickstarter/configuration/KickstarterWebConfiguration.java
+++ src/main/java/com/gojava2/kickstarter/configuration/KickstarterWebConfiguration.java
@@ -8,9 +8,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
-import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
 /**
@@ -23,9 +21,8 @@ public class KickstarterWebConfiguration extends WebMvcConfigurerAdapter {
 
 
     @Bean
-    TilesViewResolver viewResolver(){
-        TilesViewResolver viewResolver = new TilesViewResolver();
-        return viewResolver;
+    TilesViewResolver viewResolver() {
+        return new TilesViewResolver();
     }
 
     @Override
